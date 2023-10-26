525eefd6cbc80bf7170b87bc36f88e4f90b5ad9e
LAPTOP-4VT05Q2G\MY PC <athul@nivid.co>
2023-10-26 17:51:26 +0530

    font family and project basic folder stcuture is created

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_web/utils/colors.dart';
 
-void main(){
-  runApp(app)
+void main() {
+  runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Web',
+      theme: ThemeData(
+          fontFamily: 'HindSiliguri',
+          brightness: Brightness.light,
+          primaryColor: AppColors.primary),
+    );
   }
 }
 
