81ddb3f83ff81f4d1c35a99e4721046339df9339
LAPTOP-4VT05Q2G\MY PC <athul@nivid.co>
2023-10-26 17:29:17 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(app)
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(home: MyHomePage());
+//   }
+// }
+
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('LayoutBuilder Example'),
+//       ),
+//       body: LayoutBuilder(
+//         builder: (BuildContext context, BoxConstraints constraints) {
+//           if (constraints.maxWidth > 600) {
+//             return _buildWideLayout();
+//           } else {
+//             return _buildNarrowLayout();
+//           }
+//         },
+//       ),
+//     );
+//   }
+
+//   Widget _buildWideLayout() {
+//     return Center(
+//       child: Container(
+//         width: 300,
+//         height: 300,
+//         color: Colors.blue,
+//         child: const Center(
+//           child: Text(
+//             'Wide Layout',
+//             style: TextStyle(fontSize: 24, color: Colors.white),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildNarrowLayout() {
+//     return Center(
+//       child: Container(
+//         width: 200,
+//         height: 200,
+//         color: Colors.green,
+//         child: const Center(
+//           child: Text(
+//             'Narrow Layout',
+//             style: TextStyle(fontSize: 20, color: Colors.white),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
